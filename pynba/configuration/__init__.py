3f956ea4b01453ca21a322ea4719d7a866e6d535
Shuixi Li <shuixili@gmail.com>
2018-04-19 20:52:26 -0400

    added logger and configuration module

---
+++ pynba/configuration/__init__.py
@@ -0,0 +1,25 @@
+import ConfigParser
+import base64
+
+
+Config = ConfigParser.ConfigParser()
+Config.optionxform = str
+Config.read("./configuration/configuration.ini")
+
+
+def config_section_map(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                pass
+                # logger.error("skip: %s" % option)
+        except:
+            # logger.error("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+twitter_config = config_section_map("TWITTER-CONFIGURATION")
