29a07283c58d1354bc94b0f0b52ac26aa71f7b43
llance <shuixili@gmail.com>
2019-03-04 08:36:34 -0500

    fix pyc commit and remove pyc from commit

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) [2019] [Shuixi Li]
+Copyright (c) 2019 Shuixi Li
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- server/nba_app/views.py
+++ server/nba_app/views.py
@@ -3,7 +3,9 @@ from nba_app.serializers import PlayerSerializer, ShotChartSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.http import JsonResponse, HttpResponse
+from django.core.serializers import serialize
+import json
 
 
 
@@ -25,5 +27,10 @@ class ShotChartList(APIView):
     """
     def get(self, request, player_id, format=None):
         shotcharts = ShotChart.objects.filter(player_id=player_id)
-        serializer = ShotChartSerializer(shotcharts, many=True)
-        return Response(serializer.data)
+        # serializer = ShotChartSerializer(shotcharts, many=True)
+        data = serialize('json', [shotcharts, ])
+
+        struct = json.loads(data)
+        data = json.dumps(struct[0])
+
+        return HttpResponse(status=200, data=data, content_type='application/json')
